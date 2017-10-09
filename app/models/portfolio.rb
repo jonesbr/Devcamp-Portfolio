3ee437d3ba22020a3c1680ea0341cb53480feac3
Brody <jones1337br@hotmail.com>
2017-10-09 12:44:35 -0600

    integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	has_many :technologies
 	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -12,6 +13,6 @@ class Portfolio < ApplicationRecord
 
 	def set_defaults
 		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-		self.main_image ||= Placeholder.image_generator(height: '350', width: '200')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
 end
