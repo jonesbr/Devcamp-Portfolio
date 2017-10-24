44826f369291c6b8064c53e63b0f0447f67b67d6
Brody <jones1337br@hotmail.com>
2017-10-24 09:27:18 -0600

    implemented collection based partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,5 +14,6 @@ module ApplicationHelper
       	greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
       	content_tag(:p, greeting, class: "source-greeting")
     end
+  end
 
 end
